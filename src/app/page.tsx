cf2e52d6af37805910a2497e262257a718f56e11
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-04 23:34:57 +0600

    personal-info component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Sidebar from "@/components/Sidebar";
+import PersonalInfo from "@/components/PersonalInfo";
 
 export default function Home() {
   return (
@@ -8,7 +9,13 @@ export default function Home() {
       <section className={styles.section1}>
         <Sidebar active={1} />
       </section>
-      <section className={styles.section2}></section>
+      <section className={styles.section2}>
+        <PersonalInfo />
+        <section className={styles.buttons}>
+          <button className={styles.back}>Go Back</button>
+          <button className={styles.next}>Next Step</button>
+        </section>
+      </section>
     </main>
   );
 }
---
+++ src/components/PersonalInfo.tsx
@@ -0,0 +1,67 @@
+import { title } from "process";
+import styles from "./PersonalInfo.module.css";
+
+type Props = {};
+type contextProp = {
+  title: string;
+  type: string;
+  name: string;
+  id: string;
+  placeholder: string;
+};
+function Context({ title, type, name, id, placeholder }: contextProp) {
+  return (
+    <div className={styles.input}>
+      <label htmlFor="name">{title}</label>
+      <input type={type} name={name} id={id} placeholder={placeholder} />
+    </div>
+  );
+}
+const PersonalInfo = (props: Props) => {
+  const types = [
+    {
+      title: "Name",
+      type: "text",
+      name: "name",
+      id: "name",
+      placeholder: "e.g. Stephen King",
+    },
+    {
+      title: "Email",
+      type: "email",
+      name: "email",
+      id: "email",
+      placeholder: "e.g. stephenking@lorem.com",
+    },
+
+    {
+      title: "Phone Number",
+      type: "text",
+      name: "phone",
+      id: "phone",
+      placeholder: "e.g. +1 234 567 890",
+    },
+  ];
+  return (
+    <section className={styles.main}>
+      <div className={styles.headings}>
+        <h1>Personal info</h1>
+        <p>Please provide your phone, email address, and phone number.</p>
+      </div>
+      <div className={styles.allInputs}>
+        {types.map((object, index) => (
+          <Context
+            title={object.title}
+            type={object.type}
+            name={object.name}
+            id={object.id}
+            placeholder={object.placeholder}
+            key={index}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PersonalInfo;
