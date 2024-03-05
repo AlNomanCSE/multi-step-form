2db9f5bc7ecb31937f3c96b2c37ca4910eb32563
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-05 19:05:47 +0600

    Plan time and price now can change when switch the togglar

--- src/components/SelectPlan.tsx
+++ src/components/SelectPlan.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./SelectPlan.module.css";
 import Image from "next/image";
 
-type Props = {};
 type CardType = {
   src: string;
   title: string;
   pricing: number;
+  time: string;
 };
-function Card({ src, title, pricing }: CardType) {
+function Card({ src, title, pricing, time }: CardType) {
   return (
     <div className={styles.card}>
       <div>
@@ -17,32 +17,67 @@ function Card({ src, title, pricing }: CardType) {
       <div className={styles.text}>
         <h1>{title}</h1>
         <br />
-        <span>${pricing}/mo</span>
+        <span>
+          ${pricing}/{time}
+        </span>
       </div>
     </div>
   );
 }
-const SelectPlan = (props: Props) => {
-  const cardDetails = [
-    {
-      src: "/images/icon-arcade.svg",
-      title: "Arcade",
-      pricing: 9,
-    },
-    {
-      src: "/images/icon-advanced.svg",
-      title: "Advanced",
-      pricing: 12,
-    },
-    {
-      src: "/images/icon-pro.svg",
-      title: "Pro",
-      pricing: 15,
-    },
-  ];
+const cardDetailsMonthly = [
+  {
+    src: "/images/icon-arcade.svg",
+    title: "Arcade",
+    pricing: 9,
+    time: "mo",
+  },
+  {
+    src: "/images/icon-advanced.svg",
+    title: "Advanced",
+    pricing: 12,
+    time: "mo",
+  },
+  {
+    src: "/images/icon-pro.svg",
+    title: "Pro",
+    pricing: 15,
+    time: "mo",
+  },
+];
+const cardDetailsYearly = [
+  {
+    src: "/images/icon-arcade.svg",
+    title: "Arcade",
+    pricing: 90,
+    time: "yr",
+  },
+  {
+    src: "/images/icon-advanced.svg",
+    title: "Advanced",
+    pricing: 120,
+    time: "yr",
+  },
+  {
+    src: "/images/icon-pro.svg",
+    title: "Pro",
+    pricing: 150,
+    time: "yr",
+  },
+];
+
+const SelectPlan = () => {
+  const [cardDetails, setCardDetails] =
+    useState<CardType[]>(cardDetailsMonthly);
+
   function handleMonthOrYear(event: React.ChangeEvent<HTMLInputElement>) {
     const isChecked = event.target.checked;
+    if (isChecked) {
+      setCardDetails(cardDetailsYearly);
+    } else {
+      setCardDetails(cardDetailsMonthly);
+    }
   }
+
   return (
     <section className={styles.main}>
       <div className={styles.headings}>
@@ -57,16 +92,29 @@ const SelectPlan = (props: Props) => {
               title={object.title}
               pricing={object.pricing}
               key={index}
+              time={object.time}
             />
           ))}
         </div>
         <div className={styles.billingTime}>
-          <span>Monthly</span>
+          <span
+            className={
+              cardDetails == cardDetailsMonthly ? `${styles.active}` : ``
+            }
+          >
+            Monthly
+          </span>
           <label className={styles.switch}>
             <input type="checkbox" onChange={handleMonthOrYear} />
             <span className={styles.slider}></span>
           </label>
-          <span>Yearly</span>
+          <span
+            className={
+              cardDetails == cardDetailsYearly ? `${styles.active}` : ``
+            }
+          >
+            Yearly
+          </span>
         </div>
       </div>
     </section>
