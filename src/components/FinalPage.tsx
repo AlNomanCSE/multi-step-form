c0638aed78c10f8c26ba374912cfc81cd0795c7a
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-06 16:47:05 +0600

    thank you page updated

--- src/components/FinalPage.tsx
+++ src/components/FinalPage.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styles from "./FinalPage.module.css";
+import Image from "next/image";
 type Props = {};
 
 const FinalPage = (props: Props) => {
   return (
     <section className={styles.main}>
       <div className={styles.headings}>
+        <Image
+          src="images/icon-thank-you.svg"
+          width={300}
+          height={300}
+          alt="thank you image"
+          className={styles.image}
+        />
         <h1> Thank you!</h1>
         <p>
           Thanks for confirming your subscription! We hope you have fun using
